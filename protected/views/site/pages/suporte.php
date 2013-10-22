c0a6583abafdd4d264c56eae798e2dc11e322df0
dbalencar <douglas.alencar@policiacivil.am.gov.br>
2013-10-22 11:34:43 -0400

    Update: -contact,sobre +suporte

---
+++ protected/views/site/pages/suporte.php
@@ -0,0 +1,17 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - Suporte';
+$this->breadcrumbs=array(
+	'Suporte',
+);
+?>
+<h1>Suporte</h1>
+
+<br/>
+
+<p>Para informar problemas na <b>Intranet</b>, envie um e-mail para <a href="mailto:suporte@ssp.am.gov.br">suporte@ssp.am.gov.br</a>.</p>
+
+<p>Para solicitações sobre a <b>Intranet</b>, envie um e-mail para <a href="mailto:sistemas@ssp.am.gov.br">sistemas@ssp.am.gov.br</a>.</p>
+
+<p>Ramal do Departamento de Tecnologia - DETEC: <b>2009</b></p>
